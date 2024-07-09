3cca215a1da02cda8b481db78b75249a7496fe1e
Admin <admin@Tobbys-Macbook.local>
2024-07-09 17:40:25 +0100

    modify gender in signup

--- src/component/pages/Signup.jsx
+++ src/component/pages/Signup.jsx
@@ -4,20 +4,16 @@ import { getData } from 'country-list';
 
 const Signup = () => {
   const [countries, setCountries] = useState([]);
-  const [years, setYears] = useState([]);
-  const [months, setMonths] = useState([]);
-  const [days, setDays] = useState([])
+  const [gender, setGender] = useState('');
 
   useEffect(() => {
     const countries = getData().map(country => country.name);
     setCountries(countries);
-    const currentYear = new Date().getFullYear();
-    const yearsArray = Array.from({ length: currentYear - 1899 }, (_, i) => 1900 + i);
-    setYears(yearsArray);
-    const monthsArray = Array.from({ length: 12 }, (_, i) => i + 1);
-    setMonths(monthsArray);
-    const daysArray = Array.from({ length: 31 }, (_, i) => i + 1);
-    setDays(daysArray);
+ 
+    const gender = (event) =>{
+      event.preventDefault();
+      alert(`Selected gender: ${gender}`);
+    };
   }, []);
 
 
@@ -75,51 +71,55 @@ const Signup = () => {
               ))}
             </select>
           </div>
-          
-          <label className="block text-purple-900 font-bold" htmlFor="age">Age</label>
-            <div className="flex space-x-2">
-              <select
-                id="days"
-                className="w-1/3 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-600"
-                required
-              >
-                {days.map((day, index) => (
-                  <option key={index} value={day}>{day}</option>
-                ))}
-              </select>
-              <select
-                id="months"
-                className="w-1/3 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-600"
-                required
-              >
-                {months.map((month, index) => (
-                  <option key={index} value={month}>{month}</option>
-                ))}
-              </select>
-              <select
-                id="years"
-                className="w-1/3 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-600"
-                required
-              >
-                {years.map((year, index) => (
-                  <option key={index} value={year}>{year}</option>
-                ))}
-              </select>
-              
-            </div>
-            
-            <div>
-<label className="block text-purple-900 font-bold" >Addresss</label>
+          <div>
+<label className="block text-purple-900 font-bold" >Password</label>
             <input
-              id="Address"
-              type="text"
+              id="password"
+              type="password"
          class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 
-         shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="input your address"
+         shadow-sm  sm:text-sm" placeholder='input your password'
               required
             />
             </div>
+            
+          <div>
+          <label className="block mb-2 text-purple-900 font-bold">Gender</label>
+          <div className="flex items-center">
+            <label className="mr-4 text-purple-900 font-bold">
+              <input
+                type="radio"
+                name="gender"
+                value="Male"
+                className="mr-2"
+                onChange={(e) => setGender(e.target.value)}
+              />
+              Male
+            </label>
+            <label className="mr-4 text-purple-900 font-bold">
+              <input
+                type="radio"
+                name="gender"
+                value="Female"
+                className="mr-2"
+                onChange={(e) => setGender(e.target.value)}
+              />
+              Female
+            </label>
+            <label className='mr-2 text-purple-900 font-bold'>
+              <input
+                type="radio"
+                name="gender"
+                value="Other"
+                className="mr-2"
+                onChange={(e) => setGender(e.target.value)}
+              />
+              Other
+            </label>
+          </div>
+          </div>
+   
             <br />
-            <br />
+            
             
             <div className="flex justify-center mt-6">
             <button
@@ -128,13 +128,16 @@ const Signup = () => {
 required  >
     Submit
   </button>
-  <span className="text-gray-500 justify-center mt-6"></span>
-  <a href="/Landing" className="text-purple-600 hover:underline justify-center mt-6 font-bold">Login</a>
- 
   </div>
-  
-          <div>
+
+  <div className="flex flex-col items-center mt-6">
+  <span className="flex justify-center mb-2"></span>
+  <a href="/Landing" className="text-purple-700 py-2 px-4 hover:underline font-bold">
+    Already a User?
+  </a>
 </div>
+        
+
   </form>
     </div>
   </div>;
