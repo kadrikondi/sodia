9915aeb60c670a6f6b0f4072ac06980c7fa59a70
chapordp <160760952+chapordp@users.noreply.github.com>
2024-07-25 07:12:58 -0500

    Add files via upload

--- src/component/layout/Footer.jsx
+++ src/component/layout/Footer.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import Card from 'react-bootstrap/Card';
+
+
 
 const Footer = () => {
-  return <div></div>;
+  return <div>
+    <Card style={{ width: '100%', padding: '0rem 2rem', backgroundColor: '#000' }}>
+      <Card.Body>
+        <h2 style={{color:'Highlight'}} >Chat for Free</h2>
+      </Card.Body>
+        <footer className="blockquote-footer">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, praesentium.
+          </footer>
+        
+      <footer style={{padding: '0rem 1.4rem', display:'flex', justifyContent: "center"}}>
+        <Card.Link href="#">Landing</Card.Link>
+        <Card.Link href="#">CreatePost</Card.Link>
+        <Card.Link href="#">ChatApp</Card.Link>
+        <Card.Link href="#">Profile</Card.Link>
+        <Card.Link href="#">All Post</Card.Link>
+        <Card.Link href="#">SignUp</Card.Link>
+        <Card.Link href="#">TermsandPolicy</Card.Link>
+        <Card.Link href="#">About Us</Card.Link> 
+      </footer>
+      <p style={{color:'purple', display:'flex', justifyContent: "center"}}>Copyright @2024</p>
+    </Card>
+    </div>;
 };
+
 
 export default Footer;
--- src/component/layout/Header.jsx
+++ src/component/layout/Header.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-const Header = () => {
-  return <div></div>;
-};
-
+  
+  const Header = () => {
+    return (
+      
+          <Navbar expand="lg" className="bg-body-tertiary">
+            <Container style={{ width:"100%", padding:'0.5rem 0rem' }} >
+              <Navbar.Brand href="/">Chat for Free</Navbar.Brand>
+                  <Nav.Link href="/">Landing</Nav.Link>
+                  <Nav.Link href="/createpost">CreatePost</Nav.Link>
+                  <Nav.Link href="/chat">ChatApp</Nav.Link>
+                  <Nav.Link href="/profile">Profile</Nav.Link>
+                  <Nav.Link href="/allpost">All post</Nav.Link>
+                  <Nav.Link href="/signup">Signup</Nav.Link>
+                  <Nav.Link href="/terms">Termandpolicy</Nav.Link>
+                  <Nav.Link href="/aboutus">About us</Nav.Link>
+            </Container>
+          </Navbar>
+        );
+      }
+      
 export default Header;
