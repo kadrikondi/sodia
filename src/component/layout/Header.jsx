9a581722d2c36e198d89052f255bc702a7c4ba38
“bada_ayo” <ayomidebada30@gmail.com>
2024-08-05 17:28:26 -0700

    profile

--- src/component/layout/Header.jsx
+++ src/component/layout/Header.jsx
@@ -7,9 +7,9 @@ import Navbar from 'react-bootstrap/Navbar';
   const Header = () => {
     return (
       
-          <Navbar expand="lg" className="bg-body-tertiary purple-900">
-            <Container style={{ width:"100%", padding:'0.5rem 0rem' }} >
-              <Navbar.Brand href="/">Chat for Free</Navbar.Brand>
+          <Navbar expand="lg" className="bg-body-tertiary bg-purple-">
+            <Container style={{ width:"100%", padding:'0.5rem 0rem', backgroundColor: 'purple-500', color:"white" }} >
+              <Navbar.Brand href="/" className="text-white">Chat for Free</Navbar.Brand>
                   <Nav.Link href="/">Landing</Nav.Link>
                   <Nav.Link href="/createpost">CreatePost</Nav.Link>
                   <Nav.Link href="/chat">ChatApp</Nav.Link>
